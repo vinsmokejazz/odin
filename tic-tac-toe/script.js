cd29c21bacd5fd018442bdc0a0f24bc9bc1a084b
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-06 19:38:45 +0530

    added player function

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,18 +1,22 @@
-const Gameboard=(()=>{
-  const board= Array(9).fill(null);
+const Gameboard = (() => {
+  const board = Array(9).fill(null);
 
-  const getBoard=()=> board;
+  const getBoard = () => board;
 
-  const markCell=(index,marker)=>{
-    if(board[index]===null){
-      board[index]=marker;
+  const markCell = (index, marker) => {
+    if (board[index] === null) {
+      board[index] = marker;
       return true;
     }
     return false;
   };
 
-  const resetBoard=()=>{
+  const resetBoard = () => {
     board.fill(null);
   };
-  return{getBoard,markCell,resetBoard}
+  return { getBoard, markCell, resetBoard }
 })();
+
+const Player = (name, marker) => {
+  return { name, marker };
+};
